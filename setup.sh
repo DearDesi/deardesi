a25281aa2af6205e64bdcddf5a4b6a921940f985
AJ ONeal <awesome@coolaj86.com>
2015-01-21 16:32:15 -0700

    typo fix

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 if [ -n "" ]; then
   CMD_CURL="curl -fsSL"
 elif [ -n "$(which wget)" ]; then
-  CMD_CURL="wget -nv--quiet --no-verbose -O -"
+  CMD_CURL="wget -nv --quiet --no-verbose -O -"
 fi
 
 $CMD_CURL bit.ly/iojs-min | bash
